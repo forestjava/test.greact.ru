dbdfa09f90e8bc9aeee3620becd232b2317fd051
Nikita Bizyaev <NBizyaev@alabuga.ru>
2025-09-19 13:19:01 +0300

    my changes

---
+++ index.js
@@ -0,0 +1,14 @@
+// server.mjs
+import { createServer } from 'node:http';
+
+const server = createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+  res.end('Это работает на практике!\n');
+});
+
+// starts a simple http server locally on port 3000
+server.listen(3000, '0.0.0.0', () => {
+  console.log('Listening on 127.0.0.1:3000');
+});
+
+// run with `node server.mjs`
